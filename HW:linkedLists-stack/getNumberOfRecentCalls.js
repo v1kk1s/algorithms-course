4ccc534774173e36e13601ca9ad84962b2920666
v1kk1s <baheryan.vika@gmail.com>
2023-07-26 21:27:35 +0300

    Add numberOfRecentCalls hw task

---
+++ HW:linkedLists-stack/getNumberOfRecentCalls.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/number-of-recent-calls/
+
+import { Queue } from '../dataStructures/queue.js';
+
+const RecentCounter = function() {
+  this.queue = new Queue();
+  this.queueLength = 0;
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+  const startTime = t - 3000;
+  this.queue.push(t);
+  this.queueLength += 1;
+
+  while (this.queue.head.next.value < startTime) {
+    this.queue.pop();
+    this.queueLength -= 1;
+  }
+
+  console.log(this.queueLength);
+  return this.queueLength;
+};
+
+const obj = new RecentCounter()
+obj.ping(1);
+obj.ping(100);
+obj.ping(3001);
+obj.ping(3002);
